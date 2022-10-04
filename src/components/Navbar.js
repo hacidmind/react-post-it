dbf0413c7af37e411ba9ea6d61818b713fccdabf
hacidmind <abiolahafeez@gmail.com>
2022-10-04 04:31:22 +0100

    added the sign in page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import postit1 from "../image/Postit1.png";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import "../css/SignIn.css";
+
 
 const Navbar = () => {
     return (
@@ -17,7 +19,7 @@ const Navbar = () => {
                                 <Link className="nav-link text-dark" aria-current="page" to="/contact">Contact</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link text-dark" to="/signin">SignIn</Link>
+                                <Link className="nav-link text-dark" to="/signin" data-bs-toggle="modal" data-bs-target="#exampleModal">SignIn</Link>
                             </li>
                         </ul>
                         <div class="d-flex" role="search">
@@ -26,6 +28,40 @@ const Navbar = () => {
                     </div>
                 </div>
             </nav>
+
+            {/* Sign In Modal */}
+            <div class="modal fade text-center signin" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header text-center border-0">
+                            {/* <h1 class="modal-title fs-5 " id="exampleModalLabel">Welcome Back</h1> */}
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div className="container signin">
+                            <h1 className='fs-3 mb-5'>Welcome Back</h1>
+                                <form>
+                                    <div class="mb-3">
+                                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                                        <input type="email" class="form-control border-0 border-bottom" />
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="exampleFormControlInput1" class="form-label">Password</label>
+                                        <input type="password" class="form-control border-0 border-bottom" />
+                                    </div>
+                                    <div class="d-grid gap-2">
+                                        <button class="btn text-white mt-4 cont2" type="button">Continue</button>
+                                    </div>
+                                </form>
+                                <div>
+                                    <p className='my-5'>No account? <span className='text-primary'>Sign Up</span></p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
